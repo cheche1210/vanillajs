aec63249e24227ace5efd8545995ddecdeac6449
cheche1210 <invictuscheche@gmail.com>
2019-08-15 18:13:15 +0900

    first commit

---
+++ vanillajs_ex/index.js
@@ -0,0 +1,16 @@
+/*const a = 221;
+let b = a - 5;
+
+console.log(b, a);
+*/
+//let은 변수가 변할 수 있고 const는 stable, var역시 let처럼 변할 수 있다
+/*
+
+변수는 되도록 const 사용할것. let 이나 var 말고
+
+String (" "): 텍스트를 묶어줘
+
+Boolean: true = 1 / false = 0
+Number: 말 그대로 숫자
+Float: float하는 숫자. 소수점이 있는 경우?
+*/

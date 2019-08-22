159909a241ee52e841a783afd81e3c5d9f0344fc
cheche1210 <invictuscheche@gmail.com>
2019-08-22 18:43:51 +0900

    calculator

--- vanillajs_ex/calculator.js
+++ vanillajs_ex/calculator.js
@@ -1,8 +1,28 @@
 const calculator = {
   plus: function(a, b) {
     return a + b;
+  },
+  minus: function(a, b) {
+    return a - b;
+  },
+  devide: function(a, b) {
+    return a / b;
+  },
+  multiply: function(a, b) {
+    return a * b;
+  },
+  power: function(a, b) {
+    return a ** b;
   }
 };
 
-const plus = calculator.plus(5, 5);
-console.log(plus);
+const a = 150;
+const b = 30;
+
+const plus = calculator.plus(a, b);
+const minus = calculator.minus(a, b);
+const devide = calculator.devide(a, b);
+const multiply = calculator.multiply(a, b);
+const power = calculator.power(a, b);
+
+console.log(`${plus},${minus},${devide},${multiply},${power}`);

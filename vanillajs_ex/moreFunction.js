0c1fcc21a48f0806384f431850bafe7563c1cee7
cheche1210 <invictuscheche@gmail.com>
2019-08-22 18:25:24 +0900

    js commit

---
+++ vanillajs_ex/moreFunction.js
@@ -0,0 +1,9 @@
+function sayHello(name, age) {
+  return `Hello ${name} you are ${age} years old`;
+}
+
+//``백틱
+const greetHaeun = sayHello("Haeun", 20);
+//greetHaeun은 sayHello 함수의 리턴값과 같은것
+
+console.log(greetHaeun);

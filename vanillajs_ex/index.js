26ba6c683002f051d8d0e898fba1870d4319d713
cheche1210 <invictuscheche@gmail.com>
2019-08-26 21:56:08 +0900

    document update

--- vanillajs_ex/index.js
+++ vanillajs_ex/index.js
@@ -14,3 +14,14 @@ Boolean: true = 1 / false = 0
 Number: 말 그대로 숫자
 Float: float하는 숫자. 소수점이 있는 경우?
 */
+//querySelector->id로 선택해 보겠다 만일 id로 찾고싶으면 #~~class는 .~~
+//querySelector은 중요
+const title = document.querySelector("#title");
+console.log(title);
+
+//.log.error......
+//html을 DOM형태로 변형 가능
+title.innerHTML = "Hi! From JS";
+title.style.color = "red";
+console.dir(document);
+document.title = "I own you now";
